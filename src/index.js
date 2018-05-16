2097e7904f2d9bfeba13c015137f71a6070ba20d
dross15 <danielpross@mac.com>
2018-05-15 20:22:44 -0400

    Cleaned up code

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Shoe extends React.Component {
       showEmpty: false,
       buttonsHidden: false,
       showFields: false
-    })
+    });
   }
 
 /* deletes the sneaker and turns cell back to empty */
@@ -38,7 +38,7 @@ class Shoe extends React.Component {
       style: '',
       size: '',
       upcid: '',
-    })
+    });
   }
 
 /* prompts the user to fill out the form for adding the shoe */
@@ -50,7 +50,7 @@ class Shoe extends React.Component {
       style: '',
       size: '',
       upcid: ''
-    })
+    });
   }
 /* prompts the user to edit the shoe in the cell, if info is empty, sets the cell back to empty */
   editShoe(){
@@ -60,12 +60,12 @@ class Shoe extends React.Component {
         showEmpty: true,
         buttonsHidden: true,
         showForm: false,
-      })
+      });
     } else {
       this.setState({
         buttonsHidden: true,
         showForm: true
-      })
+      });
     }
   }
 
@@ -87,7 +87,7 @@ class Shoe extends React.Component {
         showEmpty: true,
         buttonsHidden: true,
         showForm: false,
-      })
+      });
     } else {
       this.setState({
         showForm: false,
@@ -100,19 +100,19 @@ class Shoe extends React.Component {
     let displayEmpty, displayButtons, displayForm, displayFields;
     /* if showEmpty is true, displays an empty cell and prompts user with options upon click on cell*/
     if (this.state.showEmpty === true) {
-      displayEmpty = <div className="empty" onClick={() => this.handleClick()}><p>{this.state.value}</p></div>
+      displayEmpty = <div className="empty" onClick={() => this.handleClick()}><p>{this.state.value}</p></div>;
     }
     /* if buttonsHidden is false, it shows the user the option buttons, add, edit, delete */
     if(this.state.buttonsHidden === false) {
-      displayButtons = <div className="buttons"><p>How would you like to proceed?</p><button type="button" onClick={() => this.addShoe()}>Add</button><button type="button" onClick={() => this.editShoe()}>Edit</button><button type="button" onClick={() => this.removeShoe()}>Remove</button></div>
+      displayButtons = <div className="buttons"><p>How would you like to proceed?</p><button type="button" onClick={() => this.addShoe()}>Add</button><button type="button" onClick={() => this.editShoe()}>Edit</button><button type="button" onClick={() => this.removeShoe()}>Remove</button></div>;
     }
     /* if showForm is true, it shows the user the form to fill out the sneaker information */
     if(this.state.showForm === true) {
-      displayForm = <form><label>Brand:<input name ="brand" value={this.state.brand} type="text" onChange={this.handleInputChange} /></label><br /><label>Style:<input name ="style" value={this.state.style} type="text" onChange={this.handleInputChange} /></label><br /><label>Size:<input name ="size" value={this.state.size} type="text" onChange={this.handleInputChange} /></label><br /><label>UPC ID:<input name ="upcid" value={this.state.upcid} type="text" onChange={this.handleInputChange} /></label><br /><button type="button" onClick={() => this.handleSubmit()}>Submit</button></form>
+      displayForm = <form><label>Brand:<input name ="brand" value={this.state.brand} type="text" onChange={this.handleInputChange} /></label><br /><label>Style:<input name ="style" value={this.state.style} type="text" onChange={this.handleInputChange} /></label><br /><label>Size:<input name ="size" value={this.state.size} type="text" onChange={this.handleInputChange} /></label><br /><label>UPC ID:<input name ="upcid" value={this.state.upcid} type="text" onChange={this.handleInputChange} /></label><br /><button type="button" onClick={() => this.handleSubmit()}>Submit</button></form>;
     }
     /* if showFields is true, it shows the user the information for the sneaker in the cell */
     if(this.state.showFields === true) {
-      displayFields = <div className="fields" onClick={() => this.handleClick()}><p>Brand: {this.state.brand}</p><p>Style: {this.state.style}</p><p>Size: {this.state.size}</p><p>UPC ID: {this.state.upcid}</p></div>
+      displayFields = <div className="fields" onClick={() => this.handleClick()}><p>Brand: {this.state.brand}</p><p>Style: {this.state.style}</p><p>Size: {this.state.size}</p><p>UPC ID: {this.state.upcid}</p></div>;
     }
     return (
     <div className="shoe">
